317807653ea3d7ffbd8ea8d4eed8b9235f1b9278
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-03-23 16:07:18 +0200

    ready

---
+++ labaVlad2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad2
+{
+    class Program
+    {
+        public static WholeNumber Init(int number)
+        {
+            return new WholeNumber(number);
+        }
+
+        public static void Read()
+        {
+            Console.Write("Введите число: ");
+            int.TryParse(Console.ReadLine(), out int number);
+            WholeNumber number1 = Init(number);
+
+            Console.Write("Введите второе число: ");
+            int.TryParse(Console.ReadLine(), out number);
+            WholeNumber number2 = Init(number);
+
+            Display(number1 - number2);
+            Display(number1 + number2);
+        }
+
+        public static void Display(WholeNumber number)
+        {
+            Console.WriteLine(number);
+        }
+
+        static void Main(string[] args)
+        {
+            Read();
+        }
+    }
+}
---
+++ labaVlad2/WholeNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad2
+{
+    public class WholeNumber
+    {
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public WholeNumber()
+        {
+            Number = 0;
+        }
+
+        public WholeNumber(int number)
+        {
+            Number = number;
+        }
+
+        public WholeNumber(WholeNumber number)
+        {
+            Number = number.Number;
+        }
+
+        public static WholeNumber operator +(WholeNumber number1, WholeNumber number2)
+        {
+            return new WholeNumber(number1.Number + number2.Number);
+        }
+
+        public static WholeNumber operator -(WholeNumber number1, WholeNumber number2)
+        {
+            return new WholeNumber(number1.Number - number2.Number);
+        }
+
+        public static implicit operator string(WholeNumber wholeNumber) => wholeNumber.ToString();
+
+        public override string ToString() => $"{Number}";
+
+    }
+}
